6d92ccdd5894d8a391896a097fe76f211de895dc
Thomas Biarneix <thomas.biarneix@voltyo.com>
2022-10-03 12:40:49 +0200

    Add image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
+      <Image
+        src="/images/profil.jpg"
+        height="150"
+        width="150"
+        alt="Thomas Biarneix PP"
+      />
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
