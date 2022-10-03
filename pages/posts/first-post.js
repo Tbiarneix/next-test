9c1233e3d55f943776bbd9e32ceccd95848139fe
Tbiarneix <thomas.biarneix@gmail.com>
2022-10-03 18:02:45 +0200

    Add css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,26 @@
+import Head from "next/head";
+import Script from "next/script";
 import Link from "next/link";
 import Image from "next/image";
+import styles from "../../styles/Button.module.css";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <Image
-        src="/images/profil.jpg"
+        src="/images/profile.jpg"
         height="150"
         width="150"
         alt="Thomas Biarneix PP"
@@ -14,7 +28,25 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+      <div className={styles.buttons}>
+        <button
+          type="button"
+          // Note how the "error" class is accessed as a property on the imported
+          // `styles` object.
+          className={styles.add}
+        >
+          <Link href="/posts">Add</Link>
+        </button>
+        <button
+          type="button"
+          // Note how the "error" class is accessed as a property on the imported
+          // `styles` object.
+          className={styles.error}
+        >
+          Destroy
+        </button>
+      </div>
+    </Layout>
   );
 }
 // https://nextjs.org/learn/basics/assets-metadata-css/assets
